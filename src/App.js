e6f5c6f32ca169f34c7b37f0b5f61b6ce5c13b86
Jahbolero <jah@home>
2024-11-16 14:05:18 +0800

    wallet connect test

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import { Box } from '@chakra-ui/react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ExplorePage from './pages/ExplorePage';
-import BottomMenu from './components/BottomMenu';
+import BottomMenu from './components/BottomMenu';    import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 function App() {
   return (
+
     <Router>
       <Box minH="100vh">
+      <TonConnectUIProvider manifestUrl="https://<YOUR_APP_URL>/tonconnect-manifest.json">
+            { /* Your app */ }
+        </TonConnectUIProvider>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/explore" element={<ExplorePage />} />
