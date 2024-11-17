cb7682c0e13f6ab4b61872c2b4058502c3b2d892
Erl Joshua Arcenal <38398477+ejacb@users.noreply.github.com>
2024-11-17 13:09:26 +0800

    Redirection

--- src/pages/Item.jsx
+++ src/pages/Item.jsx
@@ -2,14 +2,17 @@
 import Title from '@components/Title';
 import SimplePageHeader from '@components/SimplePageHeader';
 import ItemDetails from '@layout/item';
+import { useLocation } from 'react-router-dom';
 
 const Item = () => {
+    const { state } = useLocation();
+    
     return (
         <>
             <Title title="Item details" />
             <SimplePageHeader title="Item details" />
             <main>
-                <ItemDetails />
+                <ItemDetails itemData={state} />
             </main>
         </>
     );
